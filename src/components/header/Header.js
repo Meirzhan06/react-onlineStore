dcad868c8a1f907e0e1e7358da15abba283171f4
Meirzhan <difu99@mail.ru>
2024-04-08 12:06:02 +0500

    Добавил promotionHeader

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+        <div className='container'>
+            <div className={styles.headerContent}>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
